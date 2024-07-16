1c76a4d578836664930ebe880b00f937aa098275
Dy <gdy1218@gmail.com>
2024-07-17 01:52:51 +0200

    fixed ALLOWED_HOSTS

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3a^j5t!c!bk=7fq%y&-=$5_ip#6lo#gup6^-iut#rk%ua=ao5^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
